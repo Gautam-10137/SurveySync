fb06e4877b9c1a8455f832d87054488738d234de
Gautam-10137 <pahwagautam786@gmail.com>
2023-12-04 20:47:14 +0530

    Poll Form Creation

---
+++ client/src/components/Poll/PollForm.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+const PollForm = () => {
+    const [Poll,SetPoll]=useState({
+        questions:[ {
+            question:'',
+            choices:''
+                }]
+            })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PollForm
